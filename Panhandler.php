5c5d78be6de4ab8795ef4730d9d0cb30a0a6fa3a
Eric James Michael Ritz <Eric@cybersprocket.com>
2010-08-13 07:03:48 -0400

    Remove author information from header of Panhandler file
    It was added by automatically a long time ago by an Emacs snippet that
    I use.  Multiple people have worked on this file, so there is no need
    for my name to be at the top.

--- Panhandler.php
+++ Panhandler.php
@@ -4,8 +4,6 @@
 
  /* Copyright (C) 2010 Cyber Sprocket Labs <info@cybersprocket.com>         */
 
- /* Authors: Eric James Michael Ritz <Eric@cybersprocket.com>               */
-
  /* This program is free software; you can redistribute it and/or           */
  /* modify it under the terms of the GNU General Public License             */
  /* as published by the Free Software Foundation; either version 3          */
